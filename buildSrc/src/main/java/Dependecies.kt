7220b7243c02871dbbcb06de77a1afdde5100012
lezvw1e <forgithubasanalievurmat10@gmail.com>
2023-05-12 23:50:00 +0600

    first commit change gradle from groovy to kotlin

---
+++ buildSrc/src/main/java/Dependecies.kt
@@ -0,0 +1,37 @@
+object Versions {
+
+}
+
+
+object Deps {
+
+}
+
+object DaggerHilt {
+
+}
+
+object Retrofit {
+
+}
+
+object Coroutines {
+
+}
+
+object Lifecycle {
+
+}
+
+object Compose {
+
+}
+
+object Android {
+
+}
+
+object Plugins {
+    const val application = "com.android.application"
+    const val jetbrainsKotlin = "org.jetbrains.kotlin.android"
+}
